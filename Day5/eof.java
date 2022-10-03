3d6ee5f012533b249c14cc34e5e56390ccfc8873
Abhishekbhatt17 <abhishekbhatt400@gmail.com>
2022-10-03 20:23:57 +0530

    Day 5 progress

---
+++ Day5/eof.java
@@ -0,0 +1,18 @@
+package Day5;
+
+import java.util.*;
+
+public class eof {
+
+    public static void main(String []args)
+    {
+        try(Scanner sc = new Scanner(System.in)){
+        int i=1;
+        while (sc.hasNext()) {
+            String s=sc.nextLine();
+            System.out.println(i + " " + s);
+            i++;
+        }
+        }
+    }
+}
