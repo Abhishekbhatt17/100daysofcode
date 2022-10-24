c6f6868bac84d2b5922f417ddfddac4a31ef81a3
Abhishekbhatt17 <abhishekbhatt400@gmail.com>
2022-10-24 10:07:17 +0530

    Day16 Progress

--- Day16/sortedarraymerge.java
+++ Day16/sortedarraymerge.java
@@ -5,12 +5,12 @@ public class sortedarraymerge{
      public static void main(String[] arg) 
 	 {
 	 
-    int array1[]=new int[8];
+    int array1[]=new int[4];
         array1[0]=1;
         array1[1]=2;
         array1[2]=3;
         array1[3]=4;
-     int[] array2 = {2,5,7,8};
+     int[] array2 = {5,6,7,8};
         System.out.println("\nArray1: "+Arrays.toString(array1));  
 	    System.out.println("\nArray2: "+Arrays.toString(array2));
 		int m =4, n=4;
