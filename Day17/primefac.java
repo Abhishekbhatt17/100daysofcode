16be983e33a411e1f6d969eb2afe96b92a9d5da8
Abhishekbhatt17 <abhishekbhatt400@gmail.com>
2022-10-25 18:16:19 +0530

    Day17 Progress

---
+++ Day17/primefac.java
@@ -0,0 +1,32 @@
+package Day17;
+
+import java.util.*;
+class primefac {
+    public static void main(String[] args)
+    {   
+      try (Scanner scan = new Scanner(System.in)) {
+        System.out.print("Input a number: ");
+          int n = scan.nextInt();
+          if (n>0)
+          {	
+           while (n%2==0) 
+            { 
+                System.out.print(2 + " "); 
+                n /= 2; 
+            } 
+  
+            for (int i = 3; i <= Math.sqrt(n); i+= 2) 
+            { 
+                while (n%i == 0) 
+                { 
+                    System.out.print(i + " "); 
+                    n /= i; 
+                } 
+            } 
+            if (n > 2) 
+                System.out.print(n); 
+           }
+    }
+	}
+}
+
