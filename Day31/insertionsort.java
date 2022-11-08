f1b4d5d549302d4bcf84c837469d43a145bc50c6
Abhishekbhatt17 <abhishekbhatt400@gmail.com>
2022-11-08 20:43:12 +0530

    Day31 progress

---
+++ Day31/insertionsort.java
@@ -0,0 +1,37 @@
+package Day31;
+
+public class insertionsort {
+	void sort(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int key = arr[i];
+			int j = i - 1;
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
+	static void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+
+		System.out.println();
+	}
+
+	// Driver method
+	public static void main(String args[])
+	{
+		int arr[] = { 99,0,200,50,3 };
+
+		insertionsort ob = new insertionsort();
+		ob.sort(arr);
+
+		printArray(arr);
+	}
+}
+
