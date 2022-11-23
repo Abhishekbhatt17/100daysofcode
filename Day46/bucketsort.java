1e89efbf56751ca73c0e8a40f93b7e4c77184ae8
Abhishekbhatt17 <abhishekbhatt400@gmail.com>
2022-11-23 11:47:19 +0530

    Day46 progress

---
+++ Day46/bucketsort.java
@@ -0,0 +1,37 @@
+package Day46;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class bucketsort {
+  public void bucketSort(float[] arr, int n) {
+    if (n <= 0)
+      return;
+    @SuppressWarnings("unchecked")
+    ArrayList<Float>[] bucket = new ArrayList[n];
+    for (int i = 0; i < n; i++)
+      bucket[i] = new ArrayList<Float>();
+    for (int i = 0; i < n; i++) {
+      int bucketIndex = (int) arr[i] * n;
+      bucket[bucketIndex].add(arr[i]);
+    }
+    for (int i = 0; i < n; i++) {
+      Collections.sort((bucket[i]));
+    }
+
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0, size = bucket[i].size(); j < size; j++) {
+        arr[index++] = bucket[i].get(j);
+      }
+    }
+  }
+  public static void main(String[] args) {
+    bucketsort b = new bucketsort();
+    float[] arr = { (float) 0.42, (float) 0.32, (float) 0.33, (float) 0.52, (float) 0.37, (float) 0.47, (float) 0.51, (float) 0.21,(float) 0.23, (float) 0.58 };
+    b.bucketSort(arr, 10);
+
+    for (float i : arr)
+      System.out.print(i + "  ");
+  }
+}
