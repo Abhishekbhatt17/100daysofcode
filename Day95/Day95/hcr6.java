1e5736832330314c4503b8064abd361e7fb409cc
Abhishekbhatt17 <abhishekbhatt400@gmail.com>
2023-01-21 20:22:53 +0530

    Day95 progress

---
+++ Day95/Day95/hcr6.java
@@ -0,0 +1,39 @@
+package Day95;
+
+
+    import java.util.*;
+
+public class hcr6 {
+
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+            int numLines = Integer.parseInt(sc.nextLine());
+            final ArrayList<ArrayList> listArray = new ArrayList<ArrayList>();
+            for(int i = 0;i<numLines;i++){
+                int numOfIntegers = sc.nextInt();
+                ArrayList<Integer> intArrayList =  new ArrayList<Integer>();
+                for(int j=0;j<numOfIntegers;j++){
+                    intArrayList.add(new Integer(sc.nextInt()));
+                }
+                listArray.add(intArrayList);
+                sc.nextLine();
+            }
+            int numQueries = Integer.parseInt(sc.nextLine());
+            for(int i=0;i<numQueries;i++){
+                int x = sc.nextInt()-1;
+                int y = sc.nextInt()-1;
+                sc.nextLine();
+                if(x<listArray.size() && y<listArray.get(x).size()){
+                    System.out.println(listArray.get(x).get(y));
+                }else{
+                        System.out.println("ERROR!");
+                    }
+            }
+        } catch (NumberFormatException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
+
