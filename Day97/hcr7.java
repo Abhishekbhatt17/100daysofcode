a5b042ab7c7191efdf008ea594c848c695834e42
Abhishekbhatt17 <abhishekbhatt400@gmail.com>
2023-01-23 10:18:12 +0530

    Day97 progress

---
+++ Day97/hcr7.java
@@ -0,0 +1,36 @@
+package Day97;
+
+import java.util.*;
+import java.util.Scanner;
+
+public class hcr7
+{
+
+    public static void main(String[] args) {
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            int numElements = Integer.parseInt(sc.nextLine());
+            ArrayList<Integer> numList = new ArrayList<>();
+            for(int i = 0;i < numElements;i++){
+                numList.add(sc.nextInt());
+            }
+            int numQueries = sc.nextInt();
+            sc.nextLine();
+            for(int i = 0;i < numQueries;i++){
+                String queryType = sc.nextLine();
+                if(queryType.equals("Insert")){
+                    String[] queryArray = sc.nextLine().split(" ");
+                    numList.add(Integer.parseInt(queryArray[0]),Integer.parseInt(queryArray[1]));
+                }else{
+                    int removeIndex = Integer.parseInt(sc.nextLine());
+                    numList.remove(removeIndex);
+                }  
+            }
+            for(Integer num : numList){
+                System.out.print(num+" ");
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+}
